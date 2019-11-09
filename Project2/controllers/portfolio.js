9936b37ae5ba7057049d21b6bc0a9642e8239a96
Adeline Lim <adelinelim07@gmail.com>
2019-11-09 11:41:41 +0800

    implemented controller

---
+++ Project2/controllers/portfolio.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+
+// Models
+const Portfolio = require('../models/aircraft.js');
+const PortfolioSeed = require('../models/seed.js');
+
+
+//seeding data
+/*
+Portfolio.create(PortfolioSeed, (err,data)=>{
+    if (err) console.log(err.message);
+    console.log('added seed data');
+
+});*/
+
+
+//Get Route
+router.get('/', (req, res)=>{
+    Portfolio.find({},(error,allAircraft)=>{
+        res.render('index.ejs',{
+            aircraft: allAircraft
+        });
+    });
+});
+
+router.get('/new', (req, res)=>{
+    res.render('new.ejs');
+});
+
+router.post('/', (req, res)=>{
+    Portfolio.create(req.body, (error, createdAircraft)=>{
+       res.redirect('/portfolio'); 
+    });
+});
+
+router.delete('/:id', (req, res)=>{
+    Portfolio.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/portfolio');//redirect back to main portfolio page
+    });
+});
+
+router.get('/:id/edit', (req, res)=>{
+    Portfolio.findById(req.params.id, (err, foundAircraft)=>{ //find aircraft
+        res.render(
+    		'edit.ejs',
+    		{
+    			aircraft: foundAircraft //pass in found aircraft
+    		}
+    	);
+    });
+});
+
+router.get('/:id', (req, res)=>{
+    Portfolio.findById(req.params.id, (err, foundAircraft)=>{
+        res.render('show.ejs', {
+            aircraft: foundAircraft
+        });
+    });
+});
+
+router.put('/:id', (req, res)=>{
+    Portfolio.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel)=>{
+        res.redirect('/portfolio');
+    });
+});
+
+module.exports = router;
--- Project2/server.js
+++ Project2/server.js
@@ -1,89 +1,32 @@
-//imports express module & initializes express
 const express = require('express');
-const app = express();
-//include the method-override package
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-//use methodOverride.  We'll be adding a query parameter to our delete form named _method
+
+const portfolioController = require('./controllers/portfolio.js');
+
+const app = express();
+
+//middleware
 app.use(methodOverride('_method'));
-app.use(express.static('public'));
-//near the top, around other app.use() calls
 app.use(express.urlencoded({extended:false}));
 
-// Dependencies
-const mongoose = require('mongoose');
-const db = mongoose.connection;
+//use controllers and routes
+app.use('/portfolio',portfolioController);
+
+app.use(express.static('public'));
+
 // Config
 const mongoURI = 'mongodb://localhost:27017/portfolio';
-// Models
-const Portfolio = require('./models/aircraft.js');
-const PortfolioSeed = require('./models/seed.js');
+
 // Connect to Mongo
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => { 
     console.log('Mongo running at', mongoURI)
 });
 // Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
-db.on('connected', () => console.log('mongo connected: ', mongoURI))
-db.on('disconnected', () => console.log('mongo disconnected'))
-
-//seeding data
-/*
-Portfolio.create(PortfolioSeed, (err,data)=>{
-    if (err) console.log(err.message);
-    console.log('added seed data');
-
-});*/
-
-
-//Get Route
-app.get('/portfolio', (req, res)=>{
-    Portfolio.find({},(error,allAircraft)=>{
-        res.render('index.ejs',{
-            aircraft: allAircraft
-        });
-    });
+mongoose.connection.once('open', () => {
+	console.log('connected to mongo');
 });
 
-app.get('/portfolio/new', (req, res)=>{
-    res.render('new.ejs');
-});
-
-app.post('/portfolio/', (req, res)=>{
-    Portfolio.create(req.body, (error, createdAircraft)=>{
-       res.redirect('/portfolio'); 
-    });
-});
-
-app.delete('/portfolio/:id', (req, res)=>{
-    Portfolio.findByIdAndRemove(req.params.id, (err, data)=>{
-        res.redirect('/portfolio');//redirect back to main portfolio page
-    });
-});
-
-app.get('/portfolio/:id/edit', (req, res)=>{
-    Portfolio.findById(req.params.id, (err, foundAircraft)=>{ //find aircraft
-        res.render(
-    		'edit.ejs',
-    		{
-    			aircraft: foundAircraft //pass in found aircraft
-    		}
-    	);
-    });
-});
-
-app.get('/portfolio/:id', (req, res)=>{
-    Portfolio.findById(req.params.id, (err, foundAircraft)=>{
-        res.render('show.ejs', {
-            aircraft: foundAircraft
-        });
-    });
-});
-
-app.put('/portfolio/:id', (req, res)=>{
-    Portfolio.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel)=>{
-        res.redirect('/portfolio');
-    });
-});
 
 app.listen(3000, ()=>{
     console.log('listening');
